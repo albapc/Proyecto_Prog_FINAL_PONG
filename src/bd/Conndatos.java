937da46e873840de0e1d2daed8ffc77ea33a167d
aperezcesar <aperezcesar@nautilus1.danielcastelao.org>
2019-05-29 13:06:12 +0200

    metodo conexion listo

--- src/bd/Conndatos.java
+++ src/bd/Conndatos.java
@@ -5,10 +5,39 @@
  */
 package bd;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author socamporomani
  */
+
 public class Conndatos {
+    
+    static Connection con = null;
+    static Statement s = null;
+    static ResultSet rs = null;
+    
+    public static Connection conectar() {
+//        boolean conectado = false;
+        try {
+            //Cargar Driver
+            Class.forName("org.sqlite.JDBC");
+            //Conectar a la base de datos SQLite
+            con = DriverManager.getConnection("jdbc:sqlite:datos.db");
+            //Confirmamos conexion
+//            conectado = true;
+            System.out.println("Conexión establecida con el servidor");
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("ERROR DE CONEXION ---> " + ex);
+//            System.out.println(conectado);
+        }
+        return con;
+    }
+    
     
 }
